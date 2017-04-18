0be41a301d86c6107531a95d45fd80afe52344b0
ReturnOfTheJack <j162194@tafe.wa.edu.au>
2017-04-18 15:48:02 +0800

    Add files via upload
    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'public#landing'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :users
+
+  get 'login', to: 'sessions#new'
+  post'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
